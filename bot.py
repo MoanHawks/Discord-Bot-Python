1fddd92c86eb6619e45e79459100f98a61e045fc
MoanHawks <52075701+MoanHawks@users.noreply.github.com>
2020-03-07 21:52:17 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,22 @@ import asyncio
 from discord.ext import commands
 import json
 
-with open('config.json', 'r') as config_file: #load config.json
+with open('config.json', 'r') as config_file: # load config.json
     load = json.load(config_file)
-    print(load["prefix_command"])
+    prefix = load["prefix_command"]
+    token = load["token"]
+
+bot = commands.Bot(command_prefix=prefix)
+
+initial_extensions = ['cogs.ping','cogs.avatar'] # simple cogs command
+
+if __name__ == '__main__':
+    for extension in initial_extensions:
+        bot.load_extension(extension)
+
+
+@bot.event
+async def on_ready():
+    print("bot is run")
+
+bot.run(token)
